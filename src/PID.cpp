8fb725826394115c13b6f42ecdcff317718ac5f3
Citlali <gmtlali88@gmail.com>
2017-04-25 15:12:33 -0700

    Implement PID_init PID_updateError and total error with P and D only)

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <vector>
 
 using namespace std;
 
@@ -10,12 +11,33 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kpi, double Kii, double Kdi) {
+  Kp = Kpi;
+  Ki = Kii;
+  Kd = Kdi;
 }
 
+
+static std::vector<double> prev_cte;
+
 void PID::UpdateError(double cte) {
+  double tau_p = Kp;
+  double tau_d = Kd;
+  double tau_i = Ki;
+  double diff_cte;
+  prev_cte.push_back(cte);
+  diff_cte = prev_cte.back();
+
+  p_error = tau_p * cte ;
+  d_error = tau_d * diff_cte;
+//  d_error = tau_i * int_cte;
+
+
 }
 
 double PID::TotalError() {
+  double steer = -p_error - d_error;
+  return steer;
+
 }
 
