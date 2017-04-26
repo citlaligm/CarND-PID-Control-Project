3996c72a6f975316d3fdfe9d758a5e6be643a70a
Citlali <gmtlali88@gmail.com>
2017-04-25 17:04:38 -0700

    Working constant velocity

--- src/PID.cpp
+++ src/PID.cpp
@@ -4,10 +4,6 @@
 
 using namespace std;
 
-/*
-* TODO: Complete the PID class.
-*/
-
 PID::PID() {}
 
 PID::~PID() {}
@@ -26,31 +22,29 @@ void PID::UpdateError(double cte) {
   double tau_d = Kd;
   double tau_i = Ki;
   double diff_cte;
-  //double int_cte;
+  double int_cte;
 
-  //difference between the previous and current CTE for D
-
+  //difference between the previous and current CTE for D controller
   if(prev_cte.size()!=0){diff_cte = cte - prev_cte.back();}
   else{diff_cte = cte ;}
   prev_cte.push_back(cte);
 
 
-
-
-  //sum of all past CTE for I
-  //for (auto& n : prev_cte)
-  //  int_cte += n;
+  ////sum of all past CTE for I controller
+  for (auto& n : prev_cte)
+    int_cte += n;
 
   p_error = tau_p * cte ;
   d_error = tau_d * diff_cte;
-  //i_error = tau_i * int_cte;
+  i_error = tau_i * int_cte;
 
 
 }
 
 double PID::TotalError() {
-  double steer = -p_error - d_error;
+  double steer = -p_error - d_error - i_error;
   return steer;
+
 
 }
 
